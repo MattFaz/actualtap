82a6c5dc3a87d09087966cc471dcb06dd99e33ba
Matt Farrell <mfarrell@squareup.com>
2025-05-27 11:50:31 +1000

    v1.0.4

--- src/hooks/auth.js
+++ src/hooks/auth.js
@@ -1,7 +1,9 @@
 module.exports = async (fastify, opts) => {
   fastify.addHook("preHandler", async (request, reply) => {
-    if (request.headers["X-API-KEY"] !== process.env.API_KEY) {
+    const apiKey = request.headers["x-api-key"] || request.headers["X-API-KEY"];
+    if (apiKey !== process.env.API_KEY) {
       reply.code(401).send({ error: "Unauthorized" });
+      return;
     }
   });
 };
